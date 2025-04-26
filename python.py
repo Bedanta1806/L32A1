a5822648cf84c38d1782d7aedaff6361a5db05a8
Bedanta1806 <bedanta2026.ktm@ullens.edu.np>
2025-04-26 21:11:19 +0545

    Add files via upload

---
+++ python.py
@@ -0,0 +1,8 @@
+name = "BEDANTA"
+age = 1
+student_1 = True
+weigth=50.5
+print ("My name is:",name,"THE DATATYPE IS ",type(name))
+print ("AGE IS:",age,"DATAYPE IS",type(age))
+print ("AM I STUDENT OR NOT?:",student_1,"THE DATA TYPE IS",type(student_1))
+print ("I AM KG:",weigth,"THE DATA TYPE IS",type(weigth))
